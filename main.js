14e91e0ef38b5149a1f0a22584a40333be8bc56c
Aditya-Chauhan-Git <adityachauhan28062006@gmail.com>
2026-06-19 12:58:49 +0530

    Make pharma.db for testing and modify main.js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const { app, BrowserWindow } = require('electron')
+const path = require('path')
 
 function createMainWindow(){
   const mainWindow = new BrowserWindow({ width: 1200, height: 700 })
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile(path.join(__dirname, 'renderer', 'index.html'))
 }
 
 app.whenReady().then(() => {
   createMainWindow();
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
---
+++ renderer/js/search.js
@@ -0,0 +1,7 @@
+const medicineSearchElem = document.querySelector('#medicine-searchbar')
+
+medicineSearchElem.addEventListener('change', async (e)=>{
+    const value = e.target.value
+
+    
+})
